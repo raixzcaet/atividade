25e9b6e02c1a9fdea6e43971ad2d7bc6fe64f2a9
Raissa Alves <131673225+raixzcaet@users.noreply.github.com>
2023-08-16 09:46:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ("express")
+const app = express ();
+const port = 8080;
+const path=  require("path")
+const basePath= path.join(__dirname, "templates")
+
+app.get ("/" , (rq, res)  => { 
+})
+  res.sendFile(`${basePath}/index.html`)
+
+app.get ("/" , (rq, res)  => { 
+   res. send ("frutas")
+})
+
+app.get ("/abacaxi" , (req, res) => {
+})
+
+app.get ("/morango" , (req, res) => {
+})
+
+app.get ("/kiwi" , (req, res) => {
+})
+
+app.get ("/pitaya" , (req, res) => {
+})
+
+app.listen (port, () =>{
+
+console.log  (`rodando servidor na porta ${port}`);
+})
